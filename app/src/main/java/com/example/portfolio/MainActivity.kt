02b1eb08db3842beeeccb72a3bf555268406ca75
Sunhoo Park <tjsgn07@gmail.com>
2023-06-23 12:13:29 +0900

    UPDATE: Main home fragment

--- app/src/main/java/com/example/portfolio/MainActivity.kt
+++ app/src/main/java/com/example/portfolio/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.loginbutton.setOnClickListener {
             Toast.makeText(this, "버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, test::class.java)
+            val intent = Intent(this, home::class.java)
             var id = binding.edittextid.text.toString()
             var pw = binding.edittextpassword.text.toString()
             intent.putExtra("id", "${id}")
